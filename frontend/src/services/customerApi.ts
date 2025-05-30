bea9dfb839e91594052307f0ffbbe15909f9d5ca
JeevanJyot55 <109175845+JeevanJyot55@users.noreply.github.com>
2025-05-30 16:01:00 -0500

    deployemeny

--- frontend/src/services/customerApi.ts
+++ frontend/src/services/customerApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customerApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,   // must be http://localhost:3000
+  baseURL: import.meta.env.VITE_API_URL,   
   headers: { 'Content-Type': 'application/json' },
 });
 
